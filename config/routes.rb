da08a6d735e5d3d5e588b375fcb76288e38c0ed7
JD <johndavid400@gmail.com>
2013-08-17 17:56:31 -0500

    updating views

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Babybest::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'companies#index'
+  root 'brands#index'
 
   resources :products
   resources :brands
