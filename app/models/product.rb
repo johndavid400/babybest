09876a45a04846dada31556fb2963d2ce5440871
JD <johndavid400@gmail.com>
2013-08-16 23:36:49 -0500

    updates

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,11 @@
 class Product < ActiveRecord::Base
   belongs_to :type
   belongs_to :company
+  belongs_to :store
 
   def name
-    "#{company.name} #{quantity} count #{type.name} ($#{per_unit.round(2)} per unit)"
+    store_name = "from #{store.name}" if store
+    "#{company.name} #{quantity} count #{type.name} #{store_name if store_name} ($#{per_unit.round(2)} per unit)"
   end
 
   def per_unit
